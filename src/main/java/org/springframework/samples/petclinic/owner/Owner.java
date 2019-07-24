10ed3f46097b414bc0b7306c272b88f98d65ce19
HyunSangHan <gustkd3@gmail.com>
2019-07-24 09:32:55 +0900

    FirstName검색으로 변환, 앞이 일치하지 않아도 검색되도록 수정, 나이 컬럼 추가

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -61,6 +61,20 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer age;
+
     public String getAddress() {
         return this.address;
     }
